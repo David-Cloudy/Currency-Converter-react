23cc038d91283ac3956baa4cebe884f1a32cb199
David-Cloudy <dawidchmura@op.pl>
2023-08-11 11:59:12 +0200

    CurrencyConverter.test.js deleted

--- src/CurrencyConverter.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import CurrencyConverter from "./CurrencyConverter";
-
-test("renders learn react link", () => {
-  render(<CurrencyConverter />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
